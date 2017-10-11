d95ec2d3f8b33fb722272c1a643e36591a19bbfe
simda-indonesia <32692060+simda-indonesia@users.noreply.github.com>
2017-10-11 15:39:47 +0700

    rc01

---
+++ protected/vendor/kartik-v/bootstrap-tabs-x/CHANGE.md
@@ -0,0 +1,66 @@
+Change Log: `bootstrap-tabs-x`
+==============================
+
+## Version 1.3.3
+
+**Date:** 17-May-2017
+
+- (enh #26): Add `initCache` method.
+- Correct code typo in README.
+- Add github contribution and PR templates.
+- Chronological ordering of issues for change log.
+- Update copyright year to current.
+- Code optimizations.
+- (enh #24): Rename all events to begin with `tabsX:` instead of `tabsX`.
+- (enh #23): Universal Module Definition for use with CommonJS, AMD or browser globals.
+- (enh #22): Added `flushCache` method for modal scenario.
+
+## Version 1.3.2
+
+**Date:** 26-Jun-2015
+
+- (enh #19): Various tab CSS styling enhancements.
+- (enh #18): Validate `disabled` tabs.
+- (enh #17): New property `addCss` for left/right tabs.
+- (enh #16): Auto size tab height for left and right positioned tabs.
+
+## Version 1.3.1
+
+**Date:** 18-Jun-2015
+
+- (enh #14): Ability to configure success and error callbacks at plugin initialization.
+- (enh #13): Enhance tabsX events for including parameters.
+- (enh #12): Enhance auto border rendering after ajax load for vertical tabs (placed left or right).
+- (enh #11): More intelligent and optimal caching for ajax tabs.
+- (enh #10): Better validation of maxTitleLength for sideways orientation.
+- (enh #9): Add new property `ajaxSettings` to allow extending ajax configuration.
+- (enh #8): Better boolean validation of `enableCache`.
+
+## Version 1.3.0
+
+**Date:** 31-Jan-2015
+
+- (enh #7): Code cleanup and restructure for JS lint changes (using JSHint Code cleanup library).
+- (bug #6): Fix trailing comma in code.
+- (enh #5): Trigger new events on tab click, ajax beforeSend, ajax success, and ajax error.
+- (enh #4): Revamp code to enable usage as a jQuery plugin function `$.fn.tabsX`.
+- (enh #3): Incorporate a new caching object and caching functionality settings for AJAX tabs.
+
+## Version 1.2.0
+
+**Date:** 08-Nov-2014
+
+- Set release to stable in composer.json.
+- Updated CHANGE log to reflect user friendly date time formats.
+
+## Version 1.1.0
+
+**Date:** 16-Sep-2014
+
+- enh #1: Enhance and build Ajax powered tabs
+
+## Version 1.0.0
+
+**Date:** 01-Aug-2014
+
+- Initial release
---
+++ protected/vendor/kartik-v/bootstrap-tabs-x/js/bootstrap-tabs-x.js
@@ -0,0 +1,195 @@
+/*!
+ * @copyright &copy; Kartik Visweswaran, Krajee.com, 2014 - 2017
+ * @version 1.3.3
+ *
+ * Bootstrap Tabs Extended - Extended Bootstrap Tabs with ability to align tabs 
+ * in multiple ways, add borders, rotated titles, and more.
+ *
+ * For more JQuery/Bootstrap plugins and demos visit http://plugins.krajee.com
+ * For more Yii related demos visit http://demos.krajee.com
+ */
+(function (factory) {
+    "use strict";
+    //noinspection JSUnresolvedVariable
+    if (typeof define === 'function' && define.amd) { // jshint ignore:line
+        define(['jquery'], factory); // jshint ignore:line
+    } else { // noinspection JSUnresolvedVariable
+        if (typeof module === 'object' && module.exports) { // jshint ignore:line
+            // noinspection JSUnresolvedVariable
+            module.exports = factory(require('jquery')); // jshint ignore:line
+        } else {
+            factory(window.jQuery);
+        }
+    }
+}(function ($) {
+    "use strict";
+    var isEmpty = function (value, trim) {
+            return value === null || value === undefined || value.length === 0 || (trim && $.trim(value) === '');
+        },
+        TabsX = function (element, options) {
+            var self = this;
+            self.$element = $(element);
+            self.init(options);
+            self.listen();
+        };
+
+    //noinspection JSUnusedGlobalSymbols
+    TabsX.prototype = {
+        constructor: TabsX,
+        init: function (options) {
+            var self = this, $el = self.$element;
+            $.each(options, function (key, val) {
+                self[key] = val;
+            });
+            self.initCache();
+            self.enableCache = !!self.enableCache;
+            if (!isEmpty(self.addCss) && !$el.hasClass(self.addCss)) {
+                $el.addClass(self.addCss);
+            }
+            self.$pane = $el.find('.tab-pane.active');
+            self.$content = $el.find('.tab-content');
+            self.$tabs = $el.find('.nav-tabs');
+            self.isVertical = ($el.hasClass('tabs-left') || $el.hasClass('tabs-right'));
+            self.isVerticalSide = self.isVertical && $el.hasClass('tab-sideways');
+            if (self.isVertical) {
+                self.$content.css('min-height', self.$tabs.outerHeight() + 1 + 'px');
+            }
+        },
+        setTitle: function ($el) {
+            var self = this, txt = $.trim($el.text()), isVertical = self.isVertical,
+                maxLen = isEmpty($el.data('maxTitleLength')) ? self.maxTitleLength : $el.data('maxTitleLength');
+            if (isVertical && txt.length > maxLen - 2 && isEmpty($el.attr('title'))) {
+                $el.attr('title', txt);
+            }
+        },
+        listen: function () {
+            var self = this, $element = self.$element;
+            $element.find('.nav-tabs li.disabled').each(function () {
+                $(this).find('[data-toggle="tab"]').removeAttr('data-toggle');
+            });
+            $element.find('.nav-tabs li [data-toggle="dropdown"]').each(function () {
+                self.setTitle($(this));
+            });
+            $element.find('.nav-tabs li [data-toggle="tab"]').each(function () {
+                var $el = $(this), $item = $el.closest('li');
+                $item.removeAttr('data-toggle');
+                self.setTitle($el);
+                $el.on('click', function (e) {
+                    if ($item.hasClass('disabled')) {
+                        e.preventDefault();
+                        return;
+                    }
+                    var vUrl = $(this).attr("data-url"), vHash = this.hash, cacheKey = vUrl + vHash, settings;
+                    if (isEmpty(vUrl) || (self.enableCache && self.cache.exist(cacheKey))) {
+                        $el.trigger('tabsX:click');
+                        return;
+                    }
+                    e.preventDefault();
+                    var $tab = $(vHash), $pane = $(this), $paneHeader = $pane,
+                        css = $(this).attr("data-loading-class") || 'kv-tab-loading',
+                        $element = $pane.closest('.dropdown'),
+                        cbSuccess = self.successCallback[vHash] || null,
+                        cbError = self.errorCallback[vHash] || null;
+                    if (!isEmpty($element.attr('class'))) {
+                        $paneHeader = $element.find('.dropdown-toggle');
+                    }
+                    settings = $.extend(true, {}, {
+                        type: 'post',
+                        dataType: 'json',
+                        url: vUrl,
+                        beforeSend: function (jqXHR, settings) {
+                            $tab.html('<br><br><br>');
+                            $paneHeader.removeClass(css).addClass(css);
+                            $el.trigger('tabsX:beforeSend', [jqXHR, settings]);
+                        },
+                        success: function (data, status, jqXHR) {
+                            setTimeout(function () {
+                                $tab.html(data);
+                                $pane.tab('show');
+                                $paneHeader.removeClass(css);
+                                if (self.enableCache) {
+                                    self.cache.set(cacheKey);
+                                }
+                                if (cbSuccess && typeof cbSuccess === "function") {
+                                    cbSuccess(data, status, jqXHR);
+                                }
+                                $el.trigger('tabsX:success', [data, status, jqXHR]);
+                            }, 300);
+                        },
+                        error: function (jqXHR, status, message) {
+                            if (cbError && typeof cbError === "function") {
+                                cbError(jqXHR, status, message);
+                            }
+                            $el.trigger('tabsX:error', [jqXHR, status, message]);
+                        },
+                        complete: function (jqXHR, status) {
+                            $el.trigger('tabsX:click', [jqXHR, status]);
+                        }
+                    }, self.ajaxSettings);
+                    $.ajax(settings);
+                });
+            });
+        },
+        initCache: function() {
+            var self = this, t = parseFloat(self.cacheTimeout);
+            if (isNaN(t)) {
+                t = 0;
+            }
+            self.cache = {
+                data: {},
+                create: function() {
+                    return (new Date().getTime());
+                },
+                exist: function (key) {
+                    return !!self.cache.data[key] && ((self.cache.create() - self.cache.data[key]) < t);
+                },
+                set: function (key) {
+                    self.cache.data[key] = self.cache.create();
+                }
+            };
+        },
+        flushCache: function () {
+            var self = this;
+            self.cache.data = {};
+        }
+    };
+
+    $.fn.tabsX = function (option) {
+        var args = Array.apply(null, arguments), retvals = [];
+        args.shift();
+        this.each(function () {
+            var self = $(this), data = self.data('tabsX'), options = typeof option === 'object' && option;
+            if (!data) {
+                data = new TabsX(this, $.extend(true, {}, $.fn.tabsX.defaults, options, $(this).data()));
+                self.data('tabsX', data);
+            }
+            if (typeof option === 'string') {
+                retvals.push(data[option].apply(data, args));
+            }
+        });
+        switch (retvals.length) {
+            case 0:
+                return this;
+            case 1:
+                return retvals[0];
+            default:
+                return retvals;
+        }
+    };
+
+    $.fn.tabsX.defaults = {
+        enableCache: true,
+        cacheTimeout: 300000,
+        maxTitleLength: 9,
+        ajaxSettings: {},
+        successCallback: {},
+        errorCallback: {},
+        addCss: 'tabs-krajee'
+    };
+
+    $.fn.tabsX.Constructor = TabsX;
+
+    $(document).on('ready', function () {
+        $('.tabs-x').tabsX({});
+    });
+}));
